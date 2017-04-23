361652ff67d2ed7b79f498066ed0e205c38b31b6
Jovan Ivanovic <jovan.ivanovic3@gmail.com>
2017-04-23 19:18:58 +0200

    Track visited objects

---
+++ game_state.gd
@@ -0,0 +1,9 @@
+extends Node
+
+var stack = []
+
+func add_to_stack(object):
+	stack.push_back(object)
+
+func _ready():
+	pass
--- interactor.gd
+++ interactor.gd
@@ -7,11 +7,18 @@ func _ready():
 	set_fixed_process(true)
 
 func label():
-	return get_tree().get_root().get_child(0).get_node("Game/Label")
+	return main_scene().get_node("Label")
+
+func main_scene():
+	return get_tree().get_root().get_child(0).get_node("Game")
 
 func update_label():
 	var message = get_node("Message").get_text()
 	label().update_text(self, message)
+
+func add_to_global_stack():
+	var object = get_parent().get_name()
+	main_scene().get_node("GameState").add_to_stack(object)
 
 func clear_label():
 	label().clear_text(self)
@@ -27,6 +34,7 @@ func _fixed_process(delta):
 			update_label = true
 		elif(update_label):
 			update_label()
+			add_to_global_stack()
 			delayed_clear_label()
 			update_label = false
 
