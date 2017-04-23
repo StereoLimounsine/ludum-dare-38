8fe6abe35248852475a6b55a294daa5925c1eb84
Jovan Ivanovic <jovan.ivanovic3@gmail.com>
2017-04-23 14:08:13 +0200

    Make rubies pickable

--- pickable.gd
+++ pickable.gd
@@ -4,5 +4,9 @@ func _ready():
 	pass
 
 func _on_Area2D_body_enter( body ):
-	if(body.is_in_group("Player")):
-		get_parent().remove_child(self)
+	if body.is_in_group("Player"):
+		var root_children = get_tree().get_root().get_child(0).get_children()
+		for root_child in root_children:
+			if root_child.is_in_group("Levels"):
+				root_child.remove_child(get_parent())
+				break
