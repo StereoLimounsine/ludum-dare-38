99e924b26b731f96bfebd5075c52c49c824186e3
Jovan Ivanovic <jovan.ivanovic3@gmail.com>
2017-04-23 20:09:44 +0200

    Ruby finder

--- game_state.gd
+++ game_state.gd
@@ -2,6 +2,16 @@ extends Node
 
 var stack = []
 
+func main_scene():
+	return get_tree().get_root().get_child(0).get_node("Game")
+
+func get_ruby(ruby_name):
+	for child in main_scene().get_loaded_scenes().values():
+		var ruby_node = child.get_node(ruby_name)
+		if ruby_node:
+			return ruby_node
+	return null
+
 func add_to_stack(object):
 	stack.push_back(object)
 
--- interactor.gd
+++ interactor.gd
@@ -17,7 +17,7 @@ func update_label():
 	label().update_text(self, message)
 
 func add_to_global_stack():
-	var object = get_parent().get_name()
+	var object = get_parent()
 	main_scene().get_node("GameState").add_to_stack(object)
 
 func clear_label():
