1bc9180b88ad4341e7f8b2ae90c8577850abaa86
Jovan Ivanovic <jovan.ivanovic3@gmail.com>
2017-04-23 16:15:00 +0200

    Add final video

--- inventory.gd
+++ inventory.gd
@@ -1,11 +1,17 @@
 extends Node2D
 
 var rubies = []
+const rubies_total = 6
+
+func try_to_finish():
+	if rubies.size() == rubies_total:
+		get_tree().get_root().get_child(0).finish()
 
 func add_ruby(ruby):
 	var rubies_node = get_node("Rubies")
 	rubies_node.get_node(ruby).show()
 	rubies.push_back(ruby)
+	try_to_finish()
 
 func _ready():
 	pass
--- root_scene.gd
+++ root_scene.gd
@@ -9,6 +9,32 @@ const scene_paths = [
   "snow_level.tscn", "water_level.tscn"
 ]
 var loaded_scenes = {}
+var play_video = false
+
+func finish():
+	var timer = get_node("Timer")
+	timer.connect("timeout", self, "show_video")
+	timer.start()
+
+func show_video():
+	hide_children(self)
+	var video = get_node("FinalVideo")
+	video.show()
+	video.play()
+	play_video = true
+
+func try_video():
+	var video = get_node("FinalVideo")
+	if play_video && !video.is_playing():
+		video.play()
+
+func hide_children(node):
+	if !node.has_method("get_children"):
+		return
+	for child in node.get_children():
+		if child.has_method("hide"):
+			child.hide()
+		hide_children(child)
 
 func switch_scene():
 	var scene_path = scene_paths[current_index]
@@ -39,6 +65,7 @@ func _fixed_process(delta):
 	   !Input.is_key_pressed(KEY_K) && 
 	   !Input.is_key_pressed(KEY_J)):
 		switch_scene()
+	try_video()
 
 func _ready():
 	switch_scene()
